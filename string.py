c3ce1ab1f16307cf732ff12cadef8d09fa87fd60
kowsalya4 <kowsalya4@users.noreply.github.com>
2026-09-08 14:48:25 +0530

    string1

---
+++ string.py
@@ -0,0 +1,11 @@
+str="hello" #create a string
+print(str[0]) #indexing
+print(str[1])
+print(str[2])
+print(str[-1])#negative indexing
+print(str[0:3]) #slicing
+for ch in str:
+    print(ch) #iterating through string 
+for i in range(len(str)):
+    print(str[i]) #iterating through string using index
+print(len(str))#length of string
